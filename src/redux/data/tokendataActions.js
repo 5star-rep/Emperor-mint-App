094f7e7c4a6ed60b1b717aa27b18efe86822a8f0
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-02-20 14:31:13 +0100

    Add files via upload

---
+++ src/redux/data/tokendataActions.js
@@ -0,0 +1,48 @@
+// log
+import tokenstore from "../tokenstore";
+
+const fetchDataRequest = () => {
+  return {
+    type: "CHECK_DATA_REQUEST",
+  };
+};
+
+const fetchDataSuccess = (payload) => {
+  return {
+    type: "CHECK_DATA_SUCCESS",
+    payload: payload,
+  };
+};
+
+const fetchDataFailed = (payload) => {
+  return {
+    type: "CHECK_DATA_FAILED",
+    payload: payload,
+  };
+};
+
+export const fetchData2 = () => {
+  return async (dispatch) => {
+    dispatch(fetchDataRequest());
+    try {
+      let Stakers = await tokenstore
+        .getState()
+        .blockchain.smartContract.methods.Stakers()
+        .call();
+      // let cost = await store
+      //   .getState()
+      //   .blockchain.smartContract.methods.cost()
+      //   .call();
+
+      dispatch(
+        fetchDataSuccess({
+          Stakers,
+          // cost,
+        })
+      );
+    } catch (err) {
+      console.log(err);
+      dispatch(fetchDataFailed("Could not load data from contract."));
+    }
+  };
+};
