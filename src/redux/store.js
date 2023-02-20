3740729ee56516719ae3eec540e921e1d59f02fa
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-02-20 15:57:14 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { applyMiddleware, compose, createStore, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import blockchainReducer from "./blockchain/blockchainReducer";
+import tokenReducer from "./blockchain/tokenReducer";
 import dataReducer from "./data/dataReducer";
+import tokendataReducer from "./data/tokendataReducer";
 
 const rootReducer = combineReducers({
   blockchain: blockchainReducer,
